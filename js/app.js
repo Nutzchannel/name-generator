3356f3eb5ff1634036046c0076055cbc11e9f54e
nutzchannel <spencie01@gmail.com>
2015-12-14 21:49:00 -0500

    Basis

---
+++ js/app.js
@@ -0,0 +1,38 @@
+function genSur() {
+    surnames = [];
+    var pickIt = Math.floor(Math.random() * surnames.length);
+    return surnames[pickIt];
+}
+
+function genMale(final) {
+    maleNames = [];
+    surname = genSur();
+    var pickIt = Math.floor(Math.random() * maleNames.length);
+    if (final) {
+        document.getElementById('result').innerHTML = maleNames[pickIt] + " " + surname;
+    } else {
+        return maleNames[pickIt] + " " + surname;
+    }
+}
+
+function genFemale(final) {
+    femaleNames = [];
+    surname = genSur();
+    var pickIt = Math.floor(Math.random() * femaleNames.length);
+    if (final) {
+        document.getElementById('result').innerHTML = femaleNames[pickIt] + " " + surname;
+    } else {
+        return femaleNames[pickIt] + " " + surname;
+    }
+}
+
+function doesntMatter() {
+    var newGen = Math.floor((Math.random() * 2) + 1);
+    if (newGen === 1) {
+        genMale(true);
+    } else if (newGen === 2) {
+        genFemale(true);
+    } else {
+        document.getElementById("result").innerHTML = "Sorry, something went wrong. <a href='https://github.com/Nutzchannel/name-generator/issues/new'>yell at me</a>";
+    }
+}
